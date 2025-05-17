606057a7fc10917287f9ba53f29221bda847771c
rexxless <rexx@duck.com>
2025-05-17 22:14:41 +0700

    Поменял одно слово в схеме бд :)

--- project/app/Models/Feedback.php
+++ project/app/Models/Feedback.php
@@ -5,13 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-
-
 class Feedback extends Model
 {
     use HasFactory;
 
     protected $table = 'feedbacks';
+
     protected $fillable = [
         'rate',
         'author',
