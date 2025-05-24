ecd3f115a204c28f49a0630a39278057649df850
rexxless <rexx@duck.com>
2025-05-24 20:19:55 +0700

    Опять фикс после добавления фронта

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: true, // required to accept Docker connections
+    port: 5173
+  }
 })
+
