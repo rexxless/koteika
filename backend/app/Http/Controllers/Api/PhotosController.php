18c6c463e82ea6347a8a55720b83fe8d56e631e2
rexxless <rexx@duck.com>
2025-05-25 15:10:29 +0700

    Создал контроллер для фото

---
+++ backend/app/Http/Controllers/Api/PhotosController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Photo;
+use Illuminate\Http\Request;
+
+class PhotosController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Photo $photo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Photo $photo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Photo $photo)
+    {
+        //
+    }
+}
