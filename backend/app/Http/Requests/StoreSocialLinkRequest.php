ed90afcf94f916d7487ef89894612d4ba33a2693
ig0R <korkinigorttit@gmail.com>
2025-05-30 15:57:19 +0700

    Добавил проверку на уникальность соц. сети при добавлении

--- backend/app/Http/Requests/StoreSocialLinkRequest.php
+++ backend/app/Http/Requests/StoreSocialLinkRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoreSocialLinkRequest extends FormRequest
 {
@@ -22,7 +23,12 @@ class StoreSocialLinkRequest extends FormRequest
     public function rules(): array
     {
         return [
-            "social_network" => 'required|string|max:255',
+            'social_network' => [
+                'required',
+                'string',
+                'max:255',
+                'unique:social_links,social_network'
+            ],
             "url" => "required|url",
         ];
     }
@@ -33,6 +39,8 @@ class StoreSocialLinkRequest extends FormRequest
             "social_network.required" => 'Поле social_network обязательно для заполнения.',
             "social_network.string" => "Название социальной сети должно быть строкой",
             "social_network.max" => "Максимальная длина названия социальной сети 255 символов.",
+            "social_network.unique" => "Такая социальная сеть уже есть.",
+            "social_network.*" => "Не удалось загрузить социальную сеть.",
 
             "url.required" => 'Поле url обязательно для заполнения.',
             "url.url" => 'Введите корректный URL-адрес.',
